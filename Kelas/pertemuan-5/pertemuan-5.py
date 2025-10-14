c6c4e4c1adbb70b2fc981326455da9b7fcdf86be
AnnisaNurRaidah(2509106078) <annisanurraidahh@gmail.com>
2025-10-14 20:13:34 +0800

    posttest ke5

---
+++ Kelas/pertemuan-5/pertemuan-5.py
@@ -0,0 +1,51 @@
+# # # # mahasigma = ["Andi", "Budi", 20, True, 45.10,
+# # # #             ["pernan", 12, ["for real", ["adit"]]]]
+# # # # mahasigma.append("sukma")
+# # # # mahasigma.append("nayla")
+# # # # mahasigma.insert(1, "julpa")
+# # # # mahasigma[5][2][1][0] = ["dapupu", "pernan"]
+# # # # mahasigma[5][2][1].insert(0, "menkrep")
+# # # # mahasigma[5][2][1].append("menkrep")
+
+anakB2 =['julpa','doni','zuhri',["lala",['lele','lili']], 'for real']
+
+print(anakB2)
+anakB2[3][1].remove('lele')
+
+# # # print(anakB2)
+
+pinjaman = ["Andi", "Budi", "Caca", "Doni"]
+history = pinjaman.pop(3), pinjaman.pop(2)
+
+print(pinjaman)
+print(history)
+
+# # # pinjaman = ["Andi","Carmen","Caca","Dodi","Eka","Feri","Gina","Hani","Dika"]     
+# # # # print(pinjaman[1:6:4])
+# # # for i in pinjaman:
+# # #     print(i)
+
+# # pinjaman = [["Andi", "Budi", "Caca", "Dodi"], ["Eka", "Feri", "Gina", "Nisa"]]
+
+# # for i in pinjaman:
+# #     for j in i:
+# #         print(j)
+
+# # anggota = ("riyadi", 20, True, 3.96, ["APD",25],("samarinda",12))
+# # print(anggota[-3])
+
+# # studyclub = ("Data Science", "Robotics", "Multimedia", "Network")
+# # liststudy=list(studyclub)
+# # liststudy.append("web")
+
+# # studyclub = tuple (liststudy)
+# # print(studyclub)
+
+# kelas = [
+# ["Ridho", "Lian", "Nabil"],
+# ["Daffa", "Dante", "Santoso"],
+# ["Pernanda", "Riyadi", "Ahnaf"]
+# ]
+
+# (nama, nama2, nama3) = kelas
+# print(nama[0])
